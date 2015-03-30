b11d0fc39415a3e4c2be1d5888d7721cbc868ebb
Andrea Anabell Loaiza Gonzaga <andreitaloaiza0511@gmail.com>
2015-03-30 07:17:11 -0500

    DIA 4

--- Sico_IA/urls.py
+++ Sico_IA/urls.py
@@ -18,4 +18,6 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'ingreso.views.index', name='index'),
+
+    url(r'^login/$', 'ingreso.views.login', name='login'),
 )
--- ingreso/views.py
+++ ingreso/views.py
@@ -6,3 +6,6 @@ from django.template import RequestContext
 
 def index(request):
     return render_to_response('index.html', {}, context_instance=RequestContext(request))
+
+def login(request):
+    return render_to_response('login.html', {}, context_instance=RequestContext(request))
